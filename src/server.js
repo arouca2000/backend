b02a643dd971b12ee21702e48a4afd251be45bfd
arouca2000 <arouca2000@hotmail.com>
2019-04-14 21:59:37 -0300

    Adiciona variáveis ambiente

--- src/server.js
+++ src/server.js
@@ -38,6 +38,6 @@ app.use('/files', express.static(path.resolve(__dirname,'..','tmp')));
 app.use(require('./routes'));
 
 // app.listen(3333);
-server.listen(3333);  // ouve tanto requisições http como websocket
+server.listen(process.env.PORT || 3333);  // ouve tanto requisições http como websocket
 
 
